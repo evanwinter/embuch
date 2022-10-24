d2257a54d4a89108b499daa8f57490b7f715d1a2
Evan Winter <ewinter@foxtrotco.com>
2022-10-23 19:53:56 -0500

    👌 IMPROVE: Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,27 +10,14 @@ const AboutPage = () => {
     <AboutSection>
       <Row>
         <Reveal effect="fadeInUp">
+          <p className="p1">Emily is a designer based in Chicago.</p>
           <p className="p1">
-            Aliquip exercitation reprehenderit duis exercitation excepteur
-            consequat officia aliquip et non reprehenderit.
+            She spends her free time reading, drawing, practicing yoga, raising
+            two beautiful cats.
           </p>
-          <p className="p1">
-            Enim labore non excepteur eiusmod aute excepteur elit labore.
-            Laborum laborum cillum est reprehenderit esse laborum id aliqua elit
-            labore id dolor. Sit excepteur cillum consectetur cupidatat eu
-            officia est fugiat dolor voluptate proident tempor non. Ea mollit
-            reprehenderit laborum veniam magna officia tempor minim.
-          </p>
-          <a className="link" href="mailto:emilybuchberger@gmail.com">
-            email
-          </a>
         </Reveal>
         <Reveal effect="fadeInUp" delay={0.15}>
-          <img
-            style={{
-              display: "block",
-              maxHeight: "calc(100vh - var(--menu-height))",
-            }}
+          <AboutImage
             src={polaroidGif}
             alt="Polaroids of Emily"
             className="mx-auto"
@@ -40,6 +27,12 @@ const AboutPage = () => {
     </AboutSection>
   );
 };
+
+const AboutImage = styled.img`
+  display: block;
+  max-height: calc(100vh - var(--menu-height));
+  margin: 0 auto;
+`;
 
 const AboutSection = styled(Section)`
   max-height: calc(100vh - var(--menu-height));
